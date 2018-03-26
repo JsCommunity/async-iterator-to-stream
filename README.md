ab0d2c9a86464c2d61ded8e50cdcefe29199c0ab
Julien Fontanet <julien.fontanet@isonoe.net>
2018-03-26 16:11:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# async-iterator-to-stream [![Build Status](https://travis-ci.org/JsCommunity/async-iterator-to-stream.png?branch=master)](https://travis-ci.org/JsCommunity/async-iterator-to-stream)
+
+> Convert an async iterator/iterable to a readable stream
+
+Even though this library is dedicated to async iterators, it is fully
+compatible with synchronous ones.
+
+Furthermore, generators can be used to create readable stream factories!
+
+## Install
+
+Installation of the [npm package](https://npmjs.org/package/async-iterator-to-stream):
+
+```
+> npm install --save async-iterator-to-stream
+```
+
+## Usage
+
+```js
+const asyncIteratorToStream = require('async-iterator-to-stream')
+
+// sync/async iterators
+asyncIteratorToStream(new Set(['foo', 'bar']).values())
+  .pipe(output)
+
+// sync/async iterables
+asyncIteratorToStream.obj([1, 2, 3])
+  .pipe(output)
+
+// if you pass a sync/async generator, it will return a factory instead of a
+// stream
+const createRangeStream = asyncIteratorToStream.obj(function * (n) {
+  for (let i = 0; i < n; ++i) {
+    yield i
+  }
+})
+createRangeStream(10)
+  .pipe(output)
+```
+
+## Example
+
+Let's implement a simpler `fs.createReadStream` to illustrate the usage of this
+library.
+
+```js
+const asyncIteratorToStream = require('async-iterator-to-stream')
+
+// promisified fs
+const fs = require('mz/fs')
+
+const createReadStream = asyncIteratorToStream(async function * (file) {
+  const fd = await fs.open(file, 'r')
+  try {
+    let size = yield
+    while (true) {
+      const buf = Buffer.alloc(size)
+      const [n] = await fs.read(fd, buf, 0, size, null)
+      if (n < size) {
+        yield buf.slice(0, n)
+        return
+      }
+      size = yield buf
+    }
+  } finally {
+    await fs.close(fd)
+  }
+})
+
+createReadStream('foo.txt').pipe(process.stdout)
+```
+
+> If your environment does not support async generators, you may use a sync
+> generator instead and `yield` promises to wait for them.
+
+## Development
+
+```
+# Install dependencies
+> yarn
+
+# Run the tests
+> yarn test
+
+# Continuously compile
+> yarn dev
+
+# Continuously run the tests
+> yarn dev-test
+
+# Build for production
+> yarn build
+```
+
+## Contributions
+
+Contributions are *very* welcomed, either on the documentation or on
+the code.
+
+You may:
+
+- report any [issue](https://github.com/JsCommunity/async-iterator-to-stream/issues)
+  you've encountered;
+- fork and create a pull request.
+
+## License
+
+ISC © [Julien Fontanet](https://github.com/julien-f)
---
+++ src/index.js
@@ -0,0 +1,119 @@
+const { Readable } = require('readable-stream')
+
+// return the next value of the iterator but if it is a promise, resolve it and
+// reinject it
+//
+// this enables the use of a simple generator instead of an async generator
+// (which are less widely supported)
+const next = async (iterator, arg) => {
+  let cursor = iterator.next(arg)
+  if (typeof cursor.then === 'function') {
+    return cursor
+  }
+  let value
+  while (
+    !cursor.done &&
+    (value = cursor.value) != null &&
+    typeof value.then === 'function'
+  ) {
+    let success = false
+    try {
+      value = await value
+      success = true
+    } catch (error) {
+      cursor = iterator.throw(error)
+    }
+    if (success) {
+      cursor = iterator.next(value)
+    }
+  }
+  return cursor
+}
+
+const getSymbol =
+  typeof Symbol === 'function'
+    ? name => {
+      const symbol = Symbol[name]
+      return symbol !== undefined ? symbol : `@@${name}`
+    }
+    : name => `@@${name}`
+
+const $$asyncIterator = getSymbol('asyncIterator')
+const $$iterator = getSymbol('iterator')
+
+const resolveToIterator = value => {
+  let tmp
+  if (typeof (tmp = value[$$asyncIterator]) === 'function') {
+    return tmp.call(value)() // async iterable
+  }
+  if (typeof (tmp = value[$$iterator]) === 'function') {
+    return tmp.call(value) // iterable
+  }
+  return value // iterator
+}
+
+// Create a readable stream from a sync/async iterator
+//
+// If a generator is passed instead of an iterator, a factory is returned
+// instead of a plain readable stream.
+//
+// The generator can be async or can yield promises to wait for them.
+//
+// `yield` returns the `size` parameter of the next method, the generator can
+// ask for it without generating a value by yielding `undefined`.
+module.exports = function asyncIteratorToStream (iterable, options) {
+  if (typeof iterable === 'function') {
+    return function () {
+      return asyncIteratorToStream(iterable.apply(this, arguments), options)
+    }
+  }
+
+  const iterator = resolveToIterator(iterable)
+  const readable = options instanceof Readable ? options : new Readable(options)
+  readable._destroy = async (error, cb) => {
+    if (error != null) {
+      if ('throw' in iterator) {
+        try {
+          await iterator.throw(error)
+        } catch (error) {
+          return cb(error)
+        }
+      }
+    } else if ('return' in iterator) {
+      try {
+        await iterator.return()
+      } catch (error) {
+        cb(error)
+      }
+    }
+    cb(error)
+  }
+  readable._read = size => {
+    let running = false
+    const read = (readable._read = async size => {
+      if (running) {
+        return
+      }
+      running = true
+      try {
+        let canPush = true
+        do {
+          const cursor = await next(iterator, size)
+          if (cursor.done) {
+            return readable.push(null)
+          }
+          const value = cursor.value
+          if (value !== undefined) {
+            canPush = readable.push(value)
+          }
+        } while (canPush)
+      } catch (error) {
+        readable.emit('error', error)
+      } finally {
+        running = false
+      }
+    })
+    return read(size)
+  }
+  return readable
+}
