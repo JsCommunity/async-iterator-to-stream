6bdfc54eab137b311134b57b01127cff2011b7ca
Julien Fontanet <julien.fontanet@isonoe.net>
2018-07-09 13:40:28 +0200

    chore: add more tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -49,4 +49,70 @@ describe('asyncIteratorToStream', () => {
     })
     expect(await getChunks(stream)).toEqual(data)
   })
+  describe('with generators', () => {
+    it('sync', async () => {
+      const expectedThis = {}
+      const expectedArgs = ['foo', 'bar']
+      const stream = asyncIteratorToStream
+        .obj(function * (...args) {
+          // this is forwarded
+          expect(this).toBe(expectedThis)
+
+          // args are forwarded
+          expect(args).toEqual(expectedArgs)
+
+          // can yield undefined to ask for the requested size
+          expect(typeof (yield undefined)).toBe('number')
+
+          // can yield a value
+          yield 1
+
+          // can yield a promise to wait for its resolution
+          expect(yield Promise.resolve('foo')).toBe('foo')
+          try {
+            yield Promise.reject(new Error('bar'))
+            expect(false).toBe(true)
+          } catch (error) {
+            expect(error.message).toBe('bar')
+          }
+
+          yield 2
+        })
+        .apply(expectedThis, expectedArgs)
+
+      expect(await getChunks(stream)).toEqual([1, 2])
+    })
+    it('async', async () => {
+      const expectedThis = {}
+      const expectedArgs = ['foo', 'bar']
+      const stream = asyncIteratorToStream
+        .obj(async function * (...args) {
+          // this is forwarded
+          expect(this).toBe(expectedThis)
+
+          // args are forwarded
+          expect(args).toEqual(expectedArgs)
+
+          // can yield undefined to ask for the requested size
+          expect(typeof (yield undefined)).toBe('number')
+
+          // can yield a value
+          yield 1
+
+          // promises are correctly handled
+          expect(await Promise.resolve('foo')).toBe('foo')
+          try {
+            yield Promise.reject(new Error('bar'))
+            expect(false).toBe(true)
+          } catch (error) {
+            expect(error.message).toBe('bar')
+          }
+
+          yield 2
+        })
+        .apply(expectedThis, expectedArgs)
+
+      expect(await getChunks(stream)).toEqual([1, 2])
+    })
+  })
 })
